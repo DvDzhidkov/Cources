6e0fe5462c180e6f538bdf204fdac278825173b8
DvDzhidkov <dvdzidkov@gmail.com>
2023-12-29 14:34:12 +0200

    I rewrite code for function fibonachi.

---
+++ Fibonachi_2.php
@@ -0,0 +1,25 @@
+<?php
+$n = 34;
+$a = array(0,1,1);
+
+//Функцuія яка обчислює суму всіх заданих чисел Фібоначі
+function SumFibonachi($n, $a)
+{
+	
+	
+	for ($i = 3; $i < $n; $i++) {
+  $a[$i] = $a[$i-1]+$a[$i-2];
+
+}
+  
+	$Sum = 0;
+	
+	for ($i = 0; $i < $n; $i++)
+	{
+		$Sum += $a[$i];
+	}
+	echo "Sum: " . $Sum . PHP_EOL;
+	
+	
+}
+SumFibonachi(34, $a).PHP_EOL;
